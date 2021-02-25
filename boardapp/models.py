c9c7315e33790164ddac112ce3112049cac77839
TAIGA ISHIZUKA <>
2021-02-25 19:49:22 +0900

    プロジェクトの作成、APP作成　template, view, model, setting, urlsの作成、サインアップページとログインページの作成、

---
+++ boardapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class BoardModel(models.Model):
+  title = models.CharField(max_length=50)
+  content = models.TextField()
+  author = models.CharField(max_length=50)
+  snsimage = models.ImageField(upload_to='')
+  good = models.IntegerField()
+  read = models.IntegerField()
+  readtext = models.TextField()
